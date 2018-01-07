1d07c041c8bf42098f3ccba1686c93f46146df70
Lior <liorz@tx.technion.ac.il>
2018-01-08 01:06:59 +0200

    Add JWT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var app = express();
 
@@ -28,42 +28,18 @@ app.use('/users', users);
 
 app.get('/newUser', (req, res) => {
 	  let username = req.query.username || '';
-	    const password = req.query.password || '';
+	  const password = req.query.password || '';
 
-	      username = username.replace(/[!@#$%^&*]/g, '');
+	  username = username.replace(/[!@#$%^&*]/g, '');
 
-	        if (!username || !password || users.username) {
-			    return res.sendStatus(400);
-			      }
+	  if (!username || !password || users.username) {
+		    return res.sendStatus(400);
+	  }
 
-		  const salt = crypto.randomBytes(128).toString('base64');
-		    const hash = crypto.pbkdf2Sync(password, salt, 10000, 512);
-
-		      users[username] = { salt, hash };
-
-		        res.sendStatus(200);
+	var token = jwt.sign({ username: username }, 'f123984c-3c67-4a24-b84d-1e3c7fa03f14');
+        res.status(200);
+	res.json(token);
 });
-
-app.get('/auth', (req, res) => {
-	  let username = req.query.username || '';
-	    const password = req.query.password || '';
-
-	      username = username.replace(/[!@#$%^&*]/g, '');
-
-	        if (!username || !password || !users[username]) {
-			    return res.sendStatus(400);
-			      }
-
-		  const hash = crypto.pbkdf2Sync(password, users[username].salt, 10000, 512);
-
-		    if (users[username].hash.toString() === hash.toString()) {
-			        res.sendStatus(200);
-				  } else {
-					      res.sendStatus(401);
-					        }
-});
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 console.log('READING FILE: ' + process.argv[2]);
 var obj = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 var randomInt = require('random-int');
-
+var jwt = require('jsonwebtoken');
 
 var mapping = new Map();
 
@@ -12,8 +12,16 @@ for (let i = 0; i < 10000; ++i) mapping.set(i, obj);
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-	res.status(200);
-	res.json(mapping.get(randomInt(10000)));	
+	jwt.verify(req.query.token, 'f123984c-3c67-4a24-b84d-1e3c7fa03f14', (err, decoded) => {
+		if (err) {
+			console.error(err);
+			res.sendStatus(403);
+			return;
+		}
+
+		res.status(200);
+		res.json(mapping.get(randomInt(10000)));	
+	});
 });
 
 module.exports = router;
